de0b622054f9331edb45e698a88442043bef4a10
Chris Smith <luvanintendo@gmail.com>
2014-06-27 13:08:11 -0500

    Started a connect four project
    helloword.js is an example
    app.js is the start of a projct
    brewInstructions for the project

---
+++ app.js
@@ -0,0 +1,5 @@
+
+setTimeout(function userChoice() { prompt("Where would you like to move?", "Exp: 1 ,2") {alert("times up!") },5000)
+}
+computerChoice1 = Math.random();
+computerChoice2 = Math.random();
