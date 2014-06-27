06d6dfedbb983fa511a884dc38766259ee072a8c
Chris Smith <luvanintendo@gmail.com>
2014-06-27 14:07:38 -0500

    so more changes on day1 with no help

--- app.js
+++ app.js
@@ -16,18 +16,16 @@ rl.question("Where on the board would you like to move? ",
    // we are inside the call back function it gets executed after
    // the user hits 'enter'
    console.log("Did you want to move here?",answer);
-	var baz = answer;
-
+	
 
 });
 
-console.log("Your first move was", answer)
+
 // You must close the readline interface  or the program will never exit
 // so I close it after waiting 5000 ms (or 5 sec)
 setTimeout(function(){
    rl.close();
-}, 10000);
-
+}, 15000);
 
 
 
