849ece3eb2d28fe4f6c6c088ad40aa9eb9899e5c
Nathan Fair <nfair@saymedia.com>
2014-07-09 12:24:57 -0400

    added some instructions and lessend the number of moves collected

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-var readline = require('readline');
+var readline = require('readline');  // this line imports a new module and returns an object
+// how it works is a little complecated and we will get to it later, but for now know that the
+// funciton 'require' takes a string and returns an object
 
-// readline is an object witha method createInterface
+// readline is an object with a method createInterface
 // createInterface is returning an object, which we store in the variable 'rl'
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+var rl = readline.createInterface({ // create interface takes an object as its argument
+    input: process.stdin,  // with 2 properties, input and output
+    output: process.stdout // stdin and stdout < stand for standard in and standard out
 });
 
 
-var answers = [];
+var answers = [];  // I create an array for my answers
+
+// rl.question is a function that takes 2 arguments: a string, and a 'callback' function
+// what is a call back funciton you may ask? a call back is a function that is called some time in the future
+// in this case the callback passed into the question function is executed after you hit enter.
+// the callback function also gets passed a single arguement, where the value of that arguement
+// is the string you typed before you hit enter
 
 function getMove() {
-    var move = {};
+    var move = {}; // moves are objects
     console.log("Where on the board would you like to move?");
     rl.question("X:",
-        function(X) {
+        function(X) { // <<< start of callback 1
             // we are inside the call back function it gets executed after
             // the user hits 'enter'
-            rl.question("Y:", function(Y) {
-                move.y = Y;
+            rl.question("Y:", function(Y) { // << start of callback 2.
+                //This callback is called after the user hits 'enter'
+                move.y = Y; // moves have a property y
                 answers.push(move);
-                if (answers.length > 9) {
+                if (answers.length > 2) { // this is where we decide how many moves to collect
                     rl.close();
                 }
-                else {
+                else { // if we don't have enough moves, call getMove again and get another
                     getMove();
                 }
-            });
-            move.x = X;
+            }); // << start of callback 2
+            move.x = X; // moves have a property x
 
-        });
+        }); // <<< end of callback 1
 
 }
 
