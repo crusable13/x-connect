ac9d25a87ef613db92728203237dc4b999df69e8
Nathan Fair <nfair@saymedia.com>
2014-07-09 11:43:31 -0400

    workin takeMove function

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 
-setTimeout(function userChoice() { prompt("Where would you like to move?", "Exp: 1 ,2") {alert("times up!") },5000)
+
+// readline is an object witha method createInterface
+// createInterface is returning an object, which we store in the variable 'rl'
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+var answers = [];
+
+function getMove() {
+    var move = {};
+    console.log("Where on the board would you like to move?");
+    rl.question("X:",
+        function(X) {
+            // we are inside the call back function it gets executed after
+            // the user hits 'enter'
+            rl.question("Y:", function(Y) {
+                move.y = Y;
+                answers.push(move);
+                if (answers.length > 9) {
+                    rl.close();
+                }
+                else {
+                    getMove();
+                }
+            });
+            move.x = X;
+
+        });
+
 }
-computerChoice1 = Math.random();
-computerChoice2 = Math.random();
+
+getMove();
