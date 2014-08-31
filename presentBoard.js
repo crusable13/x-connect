73a85dc08abf1f896c3a480b2251b33b1ac7ff10
Nathan Fair <nfair@saymedia.com>
2014-08-31 00:37:34 -0500

    This exports present board for use in other programs

--- presentBoard.js
+++ presentBoard.js
@@ -1,40 +1,11 @@
 var readline = require('readline');  // this line imports a new module and returns an object
-// how it works is a little complecated and we will get to it later, but for now know that the
-// funciton 'require' takes a string and returns an object
-
-// readline is an object with a method createInterface
-// createInterface is returning an object, which we store in the variable 'rl'
-var rl = readline.createInterface({ // create interface takes an object as its argument
-    input: process.stdin,  // with 2 properties, input and output
-    output: process.stdout // stdin and stdout < stand for standard in and standard out
-});
-
-/*
-problems
-
-var thing = 'baz'
-thing = thing + 'bar' + '|'
-thing == 'bazbar|'
-var row1 = aBoard.board[0][0];
-row1 = row1 + aBoard.board[1][0];
-*/
-
-// problem 1 what about when a space is undefined -> ' '
-// problem 2 XXO X in between each element we need '|'
-// problme 3 X|O|X| |
-///          X|X| |O| ------------- no bar between lines
-// we also  | | | |
-//          +-+-+-+
-//          | | | |
-//          rl.write("somestrg v ping")
-
 
 aBoard = {
   xSize : 10,
   ySize : 10,
   board: [ ['X','O','X','X', , , , , , ] ,
-           ['X','O','O',   , , , , , , ], 
-           ['X',   ,'O',   , , , , , , ], 
+           ['X','O','O',   , , , , , , ],
+           ['X',   ,'O',   , , , , , , ],
            [   ,   ,   ,   , , , , , , ],
            [   ,   ,   ,   , , , , , , ],
            [   ,   ,   ,   , , , , , , ],
@@ -43,8 +14,12 @@ aBoard = {
            [   ,   ,   ,   , , , , , , ],
            [   ,   ,   ,   , , , , , , ] ]
 }
+
 function presentBoard(thisBoard){
-//  <arrayname>[0]
+    var rl = readline.createInterface({ // create interface takes an object as its argument
+        input: process.stdin,  // with 2 properties, input and output
+        output: process.stdout // stdin and stdout < stand for standard in and standard out
+    });
   for(var i = 0; i < thisBoard.board.length; i++) {
   var arrayValue = thisBoard.board[i];
     var line = '|';
@@ -62,11 +37,9 @@ function presentBoard(thisBoard){
     line = line + "\n"
     divider = divider + "+\n"
     rl.write(line);
-  rl.write(divider);  
+  rl.write(divider);
   }
+  rl.close();
 }
 
-presentBoard(aBoard);
-// I Have an idea on how to  make it better but i have no idea where i would start
-rl.close();
-
+module.exports = presentBoard;
